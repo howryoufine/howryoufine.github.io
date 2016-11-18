c2043890d99fa205a70304cb7ea5813ec5357b63
howryoufine <howryoufine@gmail.com>
2016-11-18 10:33:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-$.ajax({
-  url: 'https://api.vk.com/method/photos.get?',
-  data: {
-    owner_id: '51964331', // ID пользователя
-    album_id: 'wall' // ID альбома
-  },
-  dataType: "jsonp",
-  success: function(data) {
-    data.response.forEach(function(item) {
-      var img = item.src_xxxbig;
-      (!(img)) ? img = item.src_big : img; // проверяем, есть ли самое большое разрешение у фотографии
-      $('#container').append('<img class="item" src="' + img + '" alt="" />');
-    });
-  }
-});
 
 
 
